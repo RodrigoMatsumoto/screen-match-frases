a6a014fc2a02800f523f3eb354d02dda61b24c9b
Rodrigo Matsumoto <matsumoto.rodrigo1@gmail.com>
2024-07-04 19:15:18 -0300

    criar repository

---
+++ backend/src/main/java/com/example/screenmatch_frases/repository/FraseRepository.java
@@ -0,0 +1,16 @@
+package com.example.screenmatch_frases.repository;
+
+import com.example.screenmatch_frases.model.Frase;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface FraseRepository extends JpaRepository<Frase, Long> {
+
+  @Query("""
+        SELECT f
+        FROM Frase f
+        ORDER BY FUNCTION('RANDOM')
+        LIMIT 1
+  """)
+  Frase buscarFraseAleatoria();
+}
