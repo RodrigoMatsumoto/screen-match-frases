ac1a633fdfaaf5649b6dcd8a3d377431a884d7b9
Rodrigo Matsumoto <matsumoto.rodrigo1@gmail.com>
2024-07-04 19:14:25 -0300

    criar model

---
+++ backend/src/main/java/com/example/screenmatch_frases/model/Frase.java
@@ -0,0 +1,60 @@
+package com.example.screenmatch_frases.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "frases")
+public class Frase {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String titulo;
+
+  private String frase;
+
+  private String personagem;
+
+  private String poster;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitulo() {
+    return titulo;
+  }
+
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
+
+  public String getFrase() {
+    return frase;
+  }
+
+  public void setFrase(String texto) {
+    this.frase = texto;
+  }
+
+  public String getPersonagem() {
+    return personagem;
+  }
+
+  public void setPersonagem(String autor) {
+    this.personagem = autor;
+  }
+
+  public String getPoster() {
+    return poster;
+  }
+
+  public void setPoster(String poster) {
+    this.poster = poster;
+  }
+}
